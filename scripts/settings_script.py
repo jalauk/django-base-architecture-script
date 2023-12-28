1e1b27b01a43fa04e6ec4e2c2802b792353267e0
CharuQL <charu.dadhich@quokkalabs.com>
2023-12-28 18:48:37 +0530

    chnaged HOST to host for mdb

--- scripts/settings_script.py
+++ scripts/settings_script.py
@@ -190,7 +190,7 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
         'ENGINE': 'djongo',
         'ENFORCE_SCHEMA': False, 
         'CLIENT': {
-            'HOST': f"mongodb+srv://{env('DB_USER')}:{env('DB_PASSWORD')}@{env('DB_CLUSTER')}/{env('DB_NAME')}",
+            'host': f"mongodb+srv://{env('DB_USER')}:{env('DB_PASSWORD')}@{env('DB_CLUSTER')}/{env('DB_NAME')}",
         }
     }
 }"""
